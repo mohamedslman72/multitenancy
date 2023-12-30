3b7377c489a7cc41b50779b315dde395f8a4f859
Mohamed Salman <mohamed.slman882@gmail.com>
2023-12-30 22:01:11 +0200

    bootstrap cdn

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -7,21 +7,6 @@ use App\Repositories\TenantRepositoryInterface;
 use App\Repositories\UserRepository;
 use App\Repositories\UserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
-use App\Repositories\Contacts\{ChatInterface,
-    CommentInterface,
-    DesignInterface,
-    InvitationInterface,
-    MessageInterface,
-    TeamInterface,
-    UserInterface};
-use App\Repositories\Eloquent\{ChatRepositories,
-    CommentRepositories,
-    DesignRepositories,
-    InvitationRepositories,
-    MessageRepositories,
-    TeamRepositories,
-    UserRepositories};
-
 class RepositoryServiceProvider extends ServiceProvider
 {
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,10 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+{{--    @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
 </head>
 <body>
     <div id="app">
